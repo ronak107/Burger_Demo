bc2bdc813374a673b44299894c4eb387bf305c4a
ronak <ronak04.exhibyte@gmail.com>
2021-10-28 12:48:41 +0530

    Adding a Toolbar

--- burger/src/components/Layout/Layout.js
+++ burger/src/components/Layout/Layout.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 import Aex from '../../hoc/Aex';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = ( props ) => (
     <Aex>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+      <Toolbar />
         <main className={classes.Content}>
             {props.children}
         </main>
