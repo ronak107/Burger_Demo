9729b0a235a41fb08ba9cb60175a0896bb412f11
ronak <ronak04.exhibyte@gmail.com>
2021-10-28 15:48:47 +0530

    Navigation Iteam

--- burger/src/components/Navigation/Toolbar/Toolbar.js
+++ burger/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems"
+
 import classes from './Toolbar.css';
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
@@ -7,7 +9,7 @@ const toolbar = (props) => (
         <Logo />
         <nav>
             <ul>
-                ....
+            <NavigationItems />
             </ul>
         </nav>
     </header>
