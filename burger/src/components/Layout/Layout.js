290d5cac80da769ce92b605b38e99f0e6c77082f
ronak <ronak04.exhibyte@gmail.com>
2021-10-29 17:00:28 +0530

    adding toggle button

--- burger/src/components/Layout/Layout.js
+++ burger/src/components/Layout/Layout.js
@@ -7,15 +7,20 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
     state = {
-        showSideDrawer: true
+        showSideDrawer: false
     }
     SideDrawerClosedHandler = () => {
         this.setState({ showSideDrawer: false })
     }
+    SideDrawerToggledHandler = () => {
+        this.setState((prevstate) => {
+            return { showSideDrawer: !prevstate.showSideDrawer }
+        })
+    }
     render() {
         return (
             <Aex>
-                <Toolbar />
+                <Toolbar drawerToggleclicked={this.SideDrawerToggledHandler}/>
                 <SideDrawer open={this.state.showSideDrawer} closed={this.SideDrawerClosedHandler} />
                 <main className={classes.Content}>
                     {this.props.children}
--- burger/src/components/Navigation/Toolbar/Toolbar.js
+++ burger/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,16 +1,15 @@
 import React from "react";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems"
-
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
 import classes from './Toolbar.css';
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <Logo />
-        <nav>
-            <ul>
+        <DrawerToggle clicked={props.DrawerToggleclicked} />
+        <div className={classes.Logo}> <Logo /></div>
+
+        <nav className={classes.DesktopOnly}>
             <NavigationItems />
-            </ul>
         </nav>
     </header>
 )
