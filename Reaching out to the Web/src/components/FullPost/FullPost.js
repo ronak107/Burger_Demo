6c07f586b7dd09ef7ecb89997d010ef36b35e917
ronak <ronak04.exhibyte@gmail.com>
2021-11-01 18:30:56 +0530

    Featch data on updated

--- Reaching out to the Web/src/components/FullPost/FullPost.js
+++ Reaching out to the Web/src/components/FullPost/FullPost.js
@@ -1,8 +1,17 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 
 import './FullPost.css';
 
 class FullPost extends Component {
+    componentDidMount() {
+        if (this.props.id) {
+            axios.get('https://jsonplaceholder.typicode.com/posts/1' + this.props.id)
+       .then(response=>{
+           console.log(response);
+       })
+        }
+    }
     render() {
         let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
         if (this.props.id) {
--- Reaching out to the Web/src/containers/Blog/Blog.js
+++ Reaching out to the Web/src/containers/Blog/Blog.js
@@ -14,26 +14,27 @@ class Blog extends Component {
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                const posts = response.data.slice(0 ,4)
-                const updatedPosts = posts.map(post =>{
-                    return{
+                const posts = response.data.slice(0, 4)
+                const updatedPosts = posts.map(post => {
+                    return {
                         ...post,
-                        author:'Ronak'
+                        author: 'Ronak'
                     }
                 })
                 this.setState({ posts: updatedPosts });
-               // console.log(response)
+                // console.log(response)
             })
-      
+
     }
-    postSelectedHandler = (id)=>{
-this.setState({selectedPostId : id})
+    postSelectedHandler = (id) => {
+        this.setState({ selectedPostId: id })
     }
     render() {
         const posts = this.state.posts.map(post => {
-           return <Post key={post.id} title={post.title}
-            author={post.author}
-            clicked={()=>this.postSelectedHandler(post.id)} />
+            return <Post key={post.id}
+             title={post.title}
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)} />
         })
         return (
             <div>
