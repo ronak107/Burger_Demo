0f18e85046938ba333547ee8e9249cc5e967b34c
ronak <ronak04.exhibyte@gmail.com>
2021-11-02 14:39:03 +0530

    sending a DELETE Request

--- Reaching out to the Web/src/components/FullPost/FullPost.js
+++ Reaching out to the Web/src/components/FullPost/FullPost.js
@@ -7,17 +7,23 @@ class FullPost extends Component {
     state = {
         loadedPost: null
     }
-    componentDidUpdate() {
-        if (this.props.id) {
+    componentDidUpdate () {
+        if ( this.props.id ) {
             if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id) ) {
-                axios.get('https://jsonplaceholder.typicode.com/posts/1' + this.props.id)
-                    .then(response => {
+                axios.get( 'https://jsonplaceholder.typicode.com/posts/' + this.props.id )
+                    .then( response => {
                         // console.log(response);
-                        this.setState({ loadedPost: response.data })
-                    })
+                        this.setState( { loadedPost: response.data } );
+                    } );
             }
-
         }
+    }
+
+    deletePostHandler = () => {
+        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            .then(response => {
+                console.log(response);
+            });
     }
     render() {
         let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
@@ -30,7 +36,7 @@ class FullPost extends Component {
                     <h1>{this.state.loadedPost.title}</h1>
                     <p>{this.state.loadedPost.content}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button onClick={this.deletePostHandler}  className="Delete">Delete</button>
                     </div>
                 </div>
 
