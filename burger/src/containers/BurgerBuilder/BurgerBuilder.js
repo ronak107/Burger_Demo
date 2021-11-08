28f3ee77d690691b9a2bb18890bc48dee7b9d366
ronak <ronak04.exhibyte@gmail.com>
2021-11-08 15:08:48 +0530

    sending a POST Request

--- burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,8 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
+
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -25,7 +27,7 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4,
+        totalPrice: 10,
         purchasable: false,
         purchasing: false
     }
@@ -81,7 +83,24 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+        // alert('You continue!');
+        const order = {
+            ingredients : this.state.ingredients,
+            price : this.state.totalPrice,
+            customer: {
+                name : 'ronak vaghasiya',
+                address : {
+                    street : 'surat',
+                    zipcode: '365630',
+                    contry : 'india'
+                },
+                email:'ronak04.exhibyte@gmail.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json',order)
+        .then(response => console.log(response))
+        .catch(error => console.log(error))
     }
 
     render () {
