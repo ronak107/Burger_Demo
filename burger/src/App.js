1a9b1ea2ee209409a20f8a2c250cebcdefb9e82e
ronak <ronak04.exhibyte@gmail.com>
2021-11-09 16:40:55 +0530

    Removeing

--- burger/src/App.js
+++ burger/src/App.js
@@ -5,11 +5,20 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Layout from './hoc/Layout/Layout';
 
 class App extends Component {
+  state = {
+    show: true
+  };
+
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({show: false});
+    }, 5000);
+  }
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
+         {this.state.show ?  <BurgerBuilder /> : null}
         </Layout>
       </div>
     );
--- burger/src/hoc/withErrorHandler/withErrorHandler.js
+++ burger/src/hoc/withErrorHandler/withErrorHandler.js
@@ -3,30 +3,35 @@ import React, { Component } from 'react';
 import Modal from '../../components/UI/Modal/Modal';
 import Aex from '../Aex/Aex';
 
-const withErrorHandler = ( WrappedComponent, axios ) => {
+const withErrorHandler = (WrappedComponent, axios) => {
     return class extends Component {
         state = {
             error: null
         }
 
-        componentWillMount () {
-            axios.interceptors.request.use(req => {
-                this.setState({error: null});
+        componentWillMount() {
+            this.reqInterceptor = axios.interceptors.request.use(req => {
+                this.setState({ error: null });
                 return req;
             });
-            axios.interceptors.response.use(res => res, error => {
-                this.setState({error: error});
+            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
+                this.setState({ error: error });
             });
+        }
+        componentWillUnmount() {
+            console.log('will unmount',this.reqInterceptor , this.resInterceptor);
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
         }
 
         errorConfirmedHandler = () => {
-            this.setState({error: null});
+            this.setState({ error: null });
         }
 
-        render () {
+        render() {
             return (
                 <Aex>
-                    <Modal 
+                    <Modal
                         show={this.state.error}
                         modalClosed={this.errorConfirmedHandler}>
                         {this.state.error ? this.state.error.message : null}
